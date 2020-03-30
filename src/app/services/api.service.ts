c66e65732d6c73753968d884c5ac0a33306dcb44
thehinneh <hinneh04@gmail.com>
2020-03-30 23:54:08 +0000

    api

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -5,7 +5,7 @@ import { HttpClient, HttpHeaders } from "@angular/common/http";
   providedIn: "root"
 })
 export class ApiService {
-  baseUrl = "https://linguist.netlify.com/linguist/";
+  baseUrl = "https://linguist-api.netlify.com/linguist/";
 
   private httpHeaders = new HttpHeaders()
     .set("Content-Type", "application/json")
